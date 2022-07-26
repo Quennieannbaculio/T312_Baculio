802fe732d5ee68c2cf88b762ceccf536366ad6ff
Quennieannbaculio <79793609+Quennieannbaculio@users.noreply.github.com>
2022-07-25 23:38:11 -0700

    Add files via upload

---
+++ main.java
@@ -0,0 +1,12 @@
+
+public class main {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Degrees Rotated In One Second Is: ");
+		
+		System.out.println((3000/60)*360);
+
+	}
+    
+}
